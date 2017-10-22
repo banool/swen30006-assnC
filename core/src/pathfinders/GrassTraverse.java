7757cbfd06b8fde9eaa17246c0da640c29933528
Daniel Porteous <danielporteous1@gmail.com>
2017-10-22 17:49:29 +1100

    Factory method for selecting the appropriate TrapTraverse after Escape

--- core/src/pathfinders/GrassTraverse.java
+++ core/src/pathfinders/GrassTraverse.java
@@ -3,9 +3,11 @@ package pathfinders;
 import utilities.Coordinate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Stack;
 
 import mycontroller.Sensor;
+import tiles.TrapTile;
 
 public class GrassTraverse extends TrapTraverse {
 
@@ -13,7 +15,7 @@ public class GrassTraverse extends TrapTraverse {
     private Coordinate start;
     private Stack<IPathFinder> pathFinderStack;
 
-    public GrassTraverse(Stack<IPathFinder> pathFinderStack, Sensor sensor) {
+    public GrassTraverse(Stack<IPathFinder> pathFinderStack, Sensor sensor, HashMap<Coordinate, TrapTile> trapSection) {
         this.sensor = sensor;
         start = sensor.getPosition();
         this.pathFinderStack = pathFinderStack;
