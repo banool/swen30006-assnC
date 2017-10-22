ae39bfa17a328cbf83dc8d6d9f078bfd46f5774b
Daniel Porteous <danielporteous1@gmail.com>
2017-10-22 23:47:11 +1100

    Final submission bundle

--- bundle_submission.sh
+++ bundle_submission.sh
@@ -4,5 +4,7 @@ mkdir -p $SUBMISSION_FOLDER_NAME
 cp -r core/src/mycontroller $SUBMISSION_FOLDER_NAME
 cp -r core/src/pathfinders $SUBMISSION_FOLDER_NAME
 cp -r core/src/pathfollowers $SUBMISSION_FOLDER_NAME
+mkdir -p "$SUBMISSION_FOLDER_NAME/utilities"
+cp -r core/src/utilities/DirectionUtils.java "$SUBMISSION_FOLDER_NAME/utilities"
 
 echo "Success!"
