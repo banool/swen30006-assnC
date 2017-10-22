523136c04b1449b71c022540a16a1d1601da201b
Daniel Porteous <danielporteous1@gmail.com>
2017-10-22 22:49:18 +1100

    Script to bundle up our submission

---
+++ bundle_submission.sh
@@ -0,0 +1,12 @@
+SUBMISSION_FOLDER_NAME="other/submission_bundle"
+
+mkdir -p $SUBMISSION_FOLDER_NAME
+cp -r core/src/mycontroller $SUBMISSION_FOLDER_NAME
+cp -r core/src/pathfinders $SUBMISSION_FOLDER_NAME
+cp -r core/src/pathfollowers $SUBMISSION_FOLDER_NAME
+mkdir -p "$SUBMISSION_FOLDER_NAME/utilities"
+cp core/src/utilities/Coordinate.java "$SUBMISSION_FOLDER_NAME/utilities"
+mkdir -p "$SUBMISSION_FOLDER_NAME/world"
+cp core/src/world/WorldSpatial.java "$SUBMISSION_FOLDER_NAME/world"
+
+echo "Success!"
